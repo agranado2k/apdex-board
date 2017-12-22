148f33c3e0d3863f9bd3b99102f1b51b5a7a70ec
Arthur Granado <agranado@pluga.co>
2017-12-22 11:09:13 -0200

    Organize functions in Namespace

--- test/board.spec.js
+++ test/board.spec.js
@@ -1,9 +1,16 @@
 describe('Board', function() {
+  it('should create object', function() {
+    var board = new NewRelicChallenge.Board()
+
+    expect(board.hosts).toBeDefined();
+    expect(board.getOrCreateHostByName).toBeDefined();
+  });
+
   describe('Host', function() {
     it('should create object', function() {
       var name = '7e6272f7-098e.dakota.biz';
 
-      var host = new Host(name);
+      var host = new NewRelicChallenge.Host(name);
 
       expect(host).not.toBeNull();
       expect(host.name).toBe(name);
@@ -12,25 +19,23 @@ describe('Board', function() {
 
   describe('Application', function() {
     it('should create object', function() {
-      var json =  {
-                    "name": "Small Fresh Pants - Kautzer - Boyer, and Sons",
-                    "contributors": [
-                      "Edwin Reinger",
-                      "Ofelia Dickens",
-                      "Hilbert Cole",
-                      "Helen Kuphal",
-                      "Maurine McDermott Sr."
-                    ],
-                    "version": 7,
-                    "apdex": 68,
-                    "host": [
-                      "7e6272f7-098e.dakota.biz",
-                      "9a450527-cdd9.kareem.info",
-                      "e7bf58af-f0be.dallas.biz"
-                    ]
-                  }
-
-      var application = new Application(json);
+      var json =  {"name": "Small Fresh Pants - Kautzer - Boyer, and Sons",
+              "contributors": [
+                "Edwin Reinger",
+                "Ofelia Dickens",
+                "Hilbert Cole",
+                "Helen Kuphal",
+                "Maurine McDermott Sr."
+              ],
+              "version": 7,
+              "apdex": 68,
+              "host": [
+                "7e6272f7-098e.dakota.biz",
+                "9a450527-cdd9.kareem.info",
+                "e7bf58af-f0be.dallas.biz"
+              ]
+            };
+      var application = new NewRelicChallenge.Application(json);
 
       expect(application).not.toBeNull();
       expect(application.name).toBe(json.name);
