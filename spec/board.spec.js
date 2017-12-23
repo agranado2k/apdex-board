c19401dd88dcd05323030fa57bbd0b72f62d5a49
Arthur Granado <agranado@pluga.co>
2017-12-23 11:30:11 -0200

    Create the function addAppToHost - O(N*M), but because M is small we can considere only O(N). Also remove code from board and spec that it was from application and host

--- spec/board.spec.js
+++ spec/board.spec.js
@@ -34,71 +34,28 @@ describe('Board', function() {
     expect(board.hostsLength()).toBe(2);
   });
 
-  describe('Host', function() {
-    var app_1, host;
+  describe('Add App', function(){
+    var app_1, app_2;
 
     beforeEach(function() {
       app_1 = new NewRelicChallenge.Application(fixture.load('app_1.json'));
-      name = '7e6272f7-098e.dakota.biz';
-      host = new NewRelicChallenge.Host(name);
+      app_2 = new NewRelicChallenge.Application(fixture.load('app_2.json'));
     });
 
-    it('should create object', function() {
-      expect(host).not.toBeNull();
-      expect(host.name).toBe(name);
+    it('should add an app to a host', function() {
+      var hostName = '7e6272f7-098e.dakota.biz';
+
+      board.addAppToHost(hostName, app_1);
+
+      expect(board.hostsLength()).toBe(1);
     });
 
-    describe('addApp', function(){
-      var app_2, app_3, app_4, app_5, app_6, app_7;
+    it('should add an app to hosts', function() {
+      board.addAppToHosts(app_1)
+      board.addAppToHosts(app_2)
 
-      beforeEach(function() {
-        app_2 = new NewRelicChallenge.Application(fixture.load('app_2.json'));
-        app_3 = new NewRelicChallenge.Application(fixture.load('app_3.json'));
-        app_4 = new NewRelicChallenge.Application(fixture.load('app_4.json'));
-        app_5 = new NewRelicChallenge.Application(fixture.load('app_5.json'));
-        app_6 = new NewRelicChallenge.Application(fixture.load('app_6.json'));
-        app_7 = new NewRelicChallenge.Application(fixture.load('app_7.json'));
-      });
-
-      it('should include a app', function() {
-        host.addApp(app_1);
-
-        expect(host.apps.length).toBe(1);
-      });
-
-      it('should include a app sort by apdex', function() {
-        host.addApp(app_1);
-        host.addApp(app_3);
-        host.addApp(app_2);
-        host.addApp(app_4);
-        host.addApp(app_5);
-        host.addApp(app_6);
-        host.addApp(app_7);
-
-        expect(host.apps[0].apdex).toBe(app_4.apdex);
-        expect(host.apps[1].apdex).toBe(app_5.apdex);
-        expect(host.apps[2].apdex).toBe(app_2.apdex);
-        expect(host.apps[3].apdex).toBe(app_7.apdex);
-        expect(host.apps[4].apdex).toBe(app_3.apdex);
-        expect(host.apps[5].apdex).toBe(app_6.apdex);
-        expect(host.apps[6].apdex).toBe(app_1.apdex);
-      });
+      expect(board.hostsLength()).toBe(4);
     });
   });
 
-  describe('Application', function() {
-    var application, json;
-    beforeEach(function() {
-      json = fixture.load('app_1.json');
-      application = new NewRelicChallenge.Application(json);
-    });
-
-    it('should create object', function() {
-      expect(application).not.toBeNull();
-      expect(application.name).toBe(json.name);
-      expect(application.contributors).toBe(json.contributors);
-      expect(application.version).toBe(json.version);
-      expect(application.apdex).toBe(json.apdex);
-    });
-  });
 });
